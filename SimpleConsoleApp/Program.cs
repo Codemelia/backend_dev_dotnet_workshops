b3ae667b87658c42fba667b5727b01e420e44c20
Codemelia <ameliaw2903@gmail.com>
2026-06-18 11:26:45 +0800

    UPLOAD complete first console app

---
+++ SimpleConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// Main class
+
+Console.WriteLine("My first .NET project");
+
+Console.WriteLine("Input your name: ");
+
+string name = Console.ReadLine() ?? string.Empty;
+
+Console.WriteLine($"Hello, {name}!");
